a4734e62faea6421501a967c9be16f34d2f3a1ef
Eastcolumn <53845799+Eastcolumn@users.noreply.github.com>
2021-12-12 19:40:48 +0900

    Update lyricparser.py

--- lyricparser.py
+++ lyricparser.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+import ast
 
 lyricData = pd.read_csv("/content/drive/MyDrive/BigData_csv/tcc_ceds_music 2.csv",header=None)
 # .tsv 읽기
@@ -11,18 +12,25 @@ condition2 = (lyricData[3]=='1995')
 condition3 = (lyricData[3]=='2005')
 condition4 = (lyricData[3]=='2015')
 
-for i in lyricData.loc[condition1][5]:
-  parsedData.append(i.split(' '))
-for i in lyricData.loc[condition2][5]:
-  parsedData.append(i.split(' '))
-for i in lyricData.loc[condition3][5]:
-  parsedData.append(i.split(' '))
-for i in lyricData.loc[condition4][5]:
-  parsedData.append(i.split(' '))
+parsedData.append(lyricData.loc[condition1][5])
+parsedData.append(lyricData.loc[condition2][5])
+parsedData.append(lyricData.loc[condition3][5])
+parsedData.append(lyricData.loc[condition4][5])
 
-for i in parsedData:
-  for j in range(len(i)):
-    result.append(i[j])
+#for i in lyricData.loc[condition1][5]:
+##  parsedData.append(i.split(' '))
+##for i in lyricData.loc[condition2][5]:
+ # parsedData.append(i.split(' '))
+#for i in lyricData.loc[condition3][5]:
+#  parsedData.append(i.split(' '))
+#for i in lyricData.loc[condition4][5]:
+#  parsedData.append(i.split(' '))
 
-
-print(result)
+#for i in parsedData:
+#  for j in range(len(i)):
+#    result.append(i[j])
+result=[]
+result.extend(lyricData.loc[condition1][5].values.tolist())
+result.extend(lyricData.loc[condition2][5].values.tolist())
+result.extend(lyricData.loc[condition3][5].values.tolist())
+result.extend(lyricData.loc[condition4][5].values.tolist())
