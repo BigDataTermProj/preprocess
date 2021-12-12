4bda3f6d644aeb00dcffc1c0903a5209ca8b984c
Eastcolumn <53845799+Eastcolumn@users.noreply.github.com>
2021-12-12 18:21:11 +0900

    Update lyric parser

--- lyricparser.py
+++ lyricparser.py
@@ -2,15 +2,27 @@ import csv
 import pandas as pd
 
 lyricData = pd.read_csv("/content/drive/MyDrive/BigData_csv/tcc_ceds_music 2.csv",header=None)
-# csv 읽기
+# .tsv 읽기
 parsedData=[]
+result=[]
 
-condition = (lyricData[3]=='1985')
+condition1 = (lyricData[3]=='1985')
+condition2 = (lyricData[3]=='1995')
+condition3 = (lyricData[3]=='2005')
+condition4 = (lyricData[3]=='2015')
 
-lyricData.loc[condition][5]
-
-
-for i in lyricData.loc[condition][5]:
+for i in lyricData.loc[condition1][5]:
   parsedData.append(i.split(' '))
-print(parsedData)
-print(len(parsedData))
+for i in lyricData.loc[condition2][5]:
+  parsedData.append(i.split(' '))
+for i in lyricData.loc[condition3][5]:
+  parsedData.append(i.split(' '))
+for i in lyricData.loc[condition4][5]:
+  parsedData.append(i.split(' '))
+
+for i in parsedData:
+  for j in range(len(i)):
+    result.append(i[j])
+
+
+print(result)
